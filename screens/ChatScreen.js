507919a4b541fa210979882c3036536fd5828adb
Vincentsarsah <avioncent@yahoo.com>
2021-11-07 04:23:20 +0000

    create chat screen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -9,14 +9,45 @@ import {
   KeyboardAvoidingView,
   ScrollView,
   TextInput,
+  Keyboard,
+  TouchableNativeFeedback,
 } from "react-native";
+import * as firebase from "firebase";
+import { db, auth } from "../firebase";
 import { Avatar } from "react-native-elements";
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
 import { Platform } from "react-native";
 
 const ChatScreen = ({ navigation, route }) => {
   const [input, setInput] = useState("");
-  const sendMessage = () => {};
+  const [messages, setMessages] = useState([]);
+  const sendMessage = () => {
+    Keyboard.dismiss();
+    db.collection("chats").doc(route.params.id).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      displayName: auth.currentUser.email,
+      email: auth.currentUser.email,
+      photoURL: auth.currentUser.photoURL,
+    });
+    setInput("");
+  };
+  useLayoutEffect(() => {
+    const unsubscribe = db
+      .collection("chats")
+      .doc(route.params.id)
+      .collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setMessages(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+    return unsubscribe;
+  }, [route]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -63,27 +94,44 @@ const ChatScreen = ({ navigation, route }) => {
     });
   }, []);
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <StatusBar style="light" />
       <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        behavior={Platform.OS === "ios" && "padding"}
         style={styles.container}
         keyboardVerticalOffset={90}
       >
-        <>
-          <ScrollView>{/*hellow */}</ScrollView>
-          <View style={styles.footer}>
-            <TextInput
-              value={input}
-              onChangeText={(text) => setInput(text)}
-              placeholder="Signal Message"
-              style={styles.textInput}
-            />
-            <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
-              <Ionicons name="send" size={24} color="#2B68E6" />
-            </TouchableOpacity>
-          </View>
-        </>
+        <TouchableNativeFeedback onPress={Keyboard.dismiss}>
+          <>
+            <ScrollView>
+              {messages.map(({ id, data }) =>
+                data.email === auth.currentUser.email ? (
+                  <View key={id} style={styles.reciever}>
+                    <Avatar />
+                    <Text style={styles.recieverText}>{data.message}</Text>
+                  </View>
+                ) : (
+                  <View style={styles.sender} key={id}>
+                    <Avatar />
+                    <Text style={styles.recieverText}>{data.message}</Text>
+                  </View>
+                )
+              )}
+            </ScrollView>
+            <View style={styles.footer}>
+              <TextInput
+                value={input}
+                onChangeText={(text) => setInput(text)}
+                placeholder="Signal Message"
+                style={styles.textInput}
+                onSubmitEditing={sendMessage}
+              />
+              <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
+                <Ionicons name="send" size={24} color="#2B68E6" />
+              </TouchableOpacity>
+            </View>
+          </>
+        </TouchableNativeFeedback>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
@@ -93,8 +141,21 @@ export default ChatScreen;
 
 const styles = StyleSheet.create({
   container: {
-    
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: 30,
   },
+  sender: {},
+  reciever: {
+    padding: 15,
+    backgroundColor: "#ECECEC",
+    alignSelf: "flex-end",
+    borderRadius: 15,
+    marginBottom: 20,
+    maxWidth: "80%",
+    position: "relative",
+  },
+  recieverText: {},
   footer: {
     flexDirection: "row",
     alignItems: "center",
@@ -106,9 +167,7 @@ const styles = StyleSheet.create({
     height: 40,
     flex: 1,
     marginRight: 15,
-    borderColor: "transparent",
     backgroundColor: "#ECECEC",
-    borderWidth: 1,
     padding: 10,
     color: "gray",
     borderRadius: 20,
